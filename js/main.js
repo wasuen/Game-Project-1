c2a60cd4f35ed1196bc27fb0669ad566a2e72596
Austin <wasuen@gmail.com>
2019-07-09 14:02:50 -0700

    start work on check for win function

--- js/main.js
+++ js/main.js
@@ -24,32 +24,41 @@ const game = {
 
 
         $('.columns.empty').hover(function() {
-            const columns = $(this).data('columns')
-            const $lastAvailableCell = findLastAvailableCell(columns)
-            $lastAvailableCell.addClass(`hover-${game.player}`)
+            const columns = $(this).data('columns');
+            const $lastAvailableCell = findLastAvailableCell(columns);
+            $lastAvailableCell.addClass(`hover-${game.player}`);
         }, function() {
             $('.columns').removeClass(`hover-${game.player}`)
-        })
+        });
 
         $('.columns.empty').click(function() {
             const columns = $(this).data('columns')
+            const row = $(this).data('rows')
             let $lastAvailableCell = findLastAvailableCell(columns)
             $lastAvailableCell.removeClass('empty');
             $lastAvailableCell.addClass(game.player);
             $('.columns').removeClass(`hover-${game.player}`)
+            const winner = game.checkForWinner(row, columns);
+            if (winner){
+                alert(`Game Over! Player ${game.player} has won`);
+                return;
+            };
             if(game.playerFlag === true){
-                $('.columns').removeClass(`hover-${game.player}`)
+                $('.columns').removeClass(`hover-${game.player}`);
                 game.player = 'black';
                 game.playerFlag = false;
-                $lastAvailableCell = findLastAvailableCell(columns)
+                $lastAvailableCell = findLastAvailableCell(columns);
                 $lastAvailableCell.addClass(`hover-${game.player}`)
             } else {
-                $('.columns').removeClass(`hover-${game.player}`)
+                $('.columns').removeClass(`hover-${game.player}`);
                 game.player = 'red';
                 game.playerFlag = true;
-                $lastAvailableCell = findLastAvailableCell(columns)
+                $lastAvailableCell = findLastAvailableCell(columns);
                 $lastAvailableCell.addClass(`hover-${game.player}`)
             };
-        })
+        });
+    },
+    checkForWinner(rows, columns) {
+
     }
 }
