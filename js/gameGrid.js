fb102401c571ea5a3a8581548d7ad23ffaa5c429
Austin <wasuen@gmail.com>
2019-07-09 10:43:17 -0700

    add hover function

--- js/gameGrid.js
+++ js/gameGrid.js
@@ -4,6 +4,8 @@ class GameGrid {
         this.columns = 8;
         this.createGrid();
     }
+
+
     createGrid() {
         const $gameGrid = $('#gameGrid') 
         for (let r = 0; r < this.rows; r++){
@@ -11,10 +13,19 @@ class GameGrid {
                 .addClass('row');
             for (let c = 0; c < this.columns; c++){
                 const $columns = $('<div>')
-                    .addClass('columns');
+                    .addClass('columns empty').attr('data-columns', c).attr('data-rows', r)
                 $row.append($columns);
             }
             $gameGrid.append($row);
         }
     }
+
+    
+    // hoverEvent() {
+    //     const $gameGrid = $('#gameGrid')
+    //     $gameGrid.on('mouseenter','.columns.empty',function(){
+    //         console.log('here', this)
+    //     })
+    // }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 $('.submit').on('click', (e) => {
-    $('.submit').hide();
-    const gameGrid = new GameGrid()
+    $('.submit').css('visibility','hidden');
+    const gameGrid = new GameGrid();
+    game.hoverEvent();
+    
 });
+
+const game = {
+
+    hoverEvent() {
+        const $gameGrid = $('#gameGrid')
+        
+        function findLastAvailableCell(columns) {
+            const cells = $(`.columns[data-columns='${columns}']`)
+            for(let i = cells.length - 1; i >= 0; i--){
+                const $cell = $(cells[i]);
+                if($cell.hasClass('empty')){
+                    return $cell
+                }
+            }
+            return null;
+        }
+
+
+        $gameGrid.on('mouseenter','.columns.empty', function() {
+            const columns = $(this).data('columns')
+            const $lastAvailableCell = findLastAvailableCell(columns)
+            $lastAvailableCell.addClass('hover-red')
+        })
+    }
+
+
+}
